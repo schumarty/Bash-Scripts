e8f503fbd9ccf462337376b154d07c35b4031838
Martin Schumacher <schumarty@gmail.com>
2019-09-02 12:07:06 -0500

    Should probably start backing up these things

---
+++ random_word.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DICTIONARY="/usr/share/dict/words"
+
+#POSSIBLE_WORDS=$(wc --lines $DICTIONARY | cut --fields=1 --delimiter=" ")
+#RANDOM_NUMBER=$(shuf --input-range=1-$POSSIBLE_WORDS --head-count=1)
+#
+#sed "${RANDOM_NUMBER}q;d" $DICTIONARY
+
+# Actually read the manpage for 'shuf' and found a better way
+
+shuf --head-count=1 $DICTIONARY 
